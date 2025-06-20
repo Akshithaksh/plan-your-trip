21e1c3726494a74321a48cbc2cc331b11b022e54
Akshith3006 <saiakshith.tr@gmail.com>
2025-06-20 13:50:48 -0500

    Initial commit - added project files excluding node_modules

--- app.js
+++ app.js
@@ -3,17 +3,19 @@ const express = require('express');
 const app = express()
 const port = process.env.port || 8080
 const authRoute = require('./routes/auth-route')
+const bookRoute = require('./routes/booking-route')
+const CRUDroute = require('./routes/CRUD-route')
 const mongoose = require('mongoose')
 const bodyParser = require('body-parser')
 const cors = require('cors')
 
 mongoose.connect(
-    "mongodb+srv://akshithaksh56:pass123@plan-your-trip.t4bsyph.mongodb.net/appData",
+    "mongodb+srv://akshithaksh56:test123@plan-your-trip.7hsuik9.mongodb.net/appData",
     (err) => {
         if(err){
-            console.log("Db not connected")
+            console.log("Database not connected")
         }else{
-            console.log("Db connected")
+            console.log("Database connected")
         }
     }
 )
@@ -24,6 +26,10 @@ app.use(bodyParser.json())
 app.use(cors())
 
 app.use('/auth', authRoute);
+
+app.use('/book', bookRoute)
+
+app.use('/crud', CRUDroute)
 
 
 app.get('/', (req, res) => {
